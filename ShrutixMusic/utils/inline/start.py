2d5d6dceaac96a66d511796cd8232659519dddd4
pjpakash <blitzbeats45@gmail.com>
2025-10-25 19:25:03 +0530

    Update start.py

--- ShrutixMusic/utils/inline/start.py
+++ ShrutixMusic/utils/inline/start.py
@@ -24,13 +24,25 @@ def private_panel(_):
                 url=f"https://t.me/{nand.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["L_N_G"], callback_data="LG"),
+            InlineKeyboardButton(
+                text=_["S_B_10"],
+                url="https://telegra.ph/Privacy-Policy-10-12-225"
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(
+                text=_["S_B_6"],
+                url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_GROUP
+            ),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
     ]
     return buttons
